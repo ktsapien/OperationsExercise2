c9c0590aff79eb0da496d47d1798fc6ab11906c5
ktsapien <kelseysapien@gmail.com>
2021-10-19 19:24:52 -0500

    initial commit

---
+++ OperationsExercise2Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OperationsExercise2Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Allow user to input the radius
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+
+            //Calculate area of the circle
+            var areaOfCircle = CalculateArea(radius);
+
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle} ");
+
+
+
+
+        }
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+
+        }
+     
+    }
+}
